4aa1151e4d8b6586ea5652595fc46affa6995f5f
Raktim Parasar <raktimparasar96@gmail.com>
2023-07-28 19:05:10 +0530

    initial folder setup

---
+++ a2z/patterns/p1.js
@@ -0,0 +1,14 @@
+let n = 5;
+let string = "";
+
+// external loop for rows
+for (let i = 0; i < n; i++) {
+  // internal loop for columns
+  for (let j = 0; j < n; j++) {
+    string += "*";
+  }
+  // newline after each row
+  string += "\n";
+}
+
+console.log(string);
