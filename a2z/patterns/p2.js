930705a3f75627d81df3977436012c4ba45b3e9f
Raktim Parasar <raktimparasar96@gmail.com>
2023-08-07 13:33:42 +0530

    pattern

--- a2z/patterns/p2.js
+++ a2z/patterns/p2.js
@@ -13,7 +13,7 @@ let string = "";
 for (i = 0; i < n; i++) {
   // internal loop for columns
   for (j = 0; j <= i; j++) {
-    string += `* `;
+    string += "* ";
   }
   // newline after each row
   string += "\n";
